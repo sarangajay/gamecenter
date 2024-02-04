d75790bc54aaf39cfda6d87ba68bf8bb687a04a6
Saranga Jayatilake <Saranga.Jayatilak@ufst>
2024-02-04 11:05:05 +0100

    initial project structure

---
+++ src/main/java/com/bs/gamecenter/GamecenterApplication.java
@@ -0,0 +1,13 @@
+package com.bs.gamecenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamecenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamecenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bs/gamecenter/GamecenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bs.gamecenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamecenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
